63546d427fee77efa3d5c78fe620ca55dd45e435
sgcm14 <sgcm14@gmail.com>
2023-12-14 20:43:20 -0500

    comunicación entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,25 +3,17 @@ import PropTypes from "prop-types";
 import { AddCategory } from "./components/AddCategory";
 
 const GifExpertApp = (props) => {
-  //const categories = ["One Punch", "Samurai X", "Dragon Ball"];
   const [categories, setCategories] = useState([
     "One Punch",
     "Samurai X",
     "Dragon Ball",
   ]);
 
-  // const handleAdd = (e) => {
-  //   setCategories([...categories, "holi"]);
-  //   // setCategories(["holi", ...categories]);
-  //   // setCategories(cats =>[...cats,"holi"]);
-  // };
-
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
-    <AddCategory/>
-      {/* <button onClick={handleAdd}>Agregar</button> */}
+      <AddCategory setCategories={setCategories} />
 
       <ol>
         {categories.map((category) => {
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-export const AddCategory = () => {
+export const AddCategory = ({setCategories}) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(inputValue.trim().length>2){
+        setCategories(cats =>[...cats,inputValue]);
+        setInputValue('');
+    }
+  };
+
   return (
-    <>
-    <h2>AddCategory</h2>
-    </>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+    setCategories : PropTypes.func.isRequired
+  };
