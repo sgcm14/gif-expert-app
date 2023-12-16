aaf5916e03e2a1c88f21c15b9c9c43e8a87d079a
sgcm14 <sgcm14@gmail.com>
2023-12-15 22:13:13 -0500

    estilos a las cards

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -26,11 +26,13 @@ export const GifGrid = ({ category }) => {
   };
   
   return (
-    <div>
-      <h3>{category}</h3>
+    <>
+    <h3>{category}</h3>
+    <div className="card-grid">
       {images.map((img) => 
         <GifGridItem key={img.id} {...img}/>
       )}
     </div>
+    </>
   );
 };
